f68a7c897240f69772457d5f7a69a9c715fdf6dd
highskore <luka@racki.dev>
2025-09-15 12:10:57 +0200

    feat: add receive event

--- contracts/utils/NexusProxy.sol
+++ contracts/utils/NexusProxy.sol
@@ -9,6 +9,9 @@ import { Initializable } from "../lib/Initializable.sol";
 /// @dev A proxy contract that uses the ERC1967 upgrade pattern and sets the initializable flag
 ///      in the constructor to prevent reinitialization
 contract NexusProxy is Proxy {
+    /// @notice Emitted when the contract receives Ether
+    event NexusReceived(address indexed sender, uint256 amount);
+
     constructor(address implementation, bytes memory data) payable {
         Initializable.setInitializable();
         ERC1967Utils.upgradeToAndCall(implementation, data);
@@ -18,5 +21,18 @@ contract NexusProxy is Proxy {
         return ERC1967Utils.getImplementation();
     }
 
-    receive() external payable { }
+    receive() external payable {
+        assembly {
+            // Store msg.value in memory
+            mstore(0x00, callvalue())
+
+            // Emit "NexusReceived(address indexed,uint256)
+            log2(
+                0x00, // Memory offset
+                0x20, // Size
+                0xa77be744658ed76e5e1397645e54770ffc61af32ea6b1238900f1d3df79aa3f2, // Event sig
+                caller() // Indexed sender
+            )
+        }
+    }
 }
